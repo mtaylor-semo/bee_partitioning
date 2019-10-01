432fec5dd5f4c27e9bcfa26f7cf129c4993c9534
Mike Taylor <mtaylor@semo.edu>
2019-10-01 13:47:33 -0500

    Initial commit

---
+++ proboscis_lengths.R
@@ -0,0 +1,18 @@
+## Bee proboscis length
+
+## Simualting values shown in Table 1 of 
+## Pleasants 1980. Ecology 61: 1446-1459.
+bifarius_queen <- rnorm(mean = 8.38, sd = 0.1, n = 20)
+bifarius_worker <- rnorm(mean = 5.75, sd = 0.1, n = 20)
+
+kirbyellus_queen <- rnorm(mean = 12.11, sd = 0.1, n = 20)
+kirbyellus_worker <- rnorm(mean = 9.36, sd = 0.1, n = 20)
+
+appositus_queen <- rnorm(mean = 12.81, sd = 0.1, n = 20)
+appositus_worker <- rnorm(mean = 10.48, sd = 0.1, n = 20)
+
+
+# Simulating mean corolla depth also in Table 1.
+bifarius_corolla <- rnorm(mean = 5.38, sd = 0.1, n = 20)
+kirbyellus_corolla <- rnorm(mean = 14.18, sd = 0.1, n = 20)
+appositus_corolla <- rnorm(mean = 14.11, sd = 0.1, n = 20)
---
+++ proboscis_v_corolla.R
@@ -0,0 +1,25 @@
+## Read and plot proboscis length vs corolla length
+## Data scanned from Figure 5 of Pyke et al. 1982
+## Data produced via WebPlatDigitizer
+
+
+library(tidyverse)
+library(ggthemes)
+
+prob_raw <- read_csv("proboscis_corolla.csv")
+prob_raw %>% 
+  ggplot(aes(x = proboscis,
+             y = corolla)) +
+  geom_point() +
+  geom_smooth(method = "lm",
+              se = FALSE,
+              col = "gray50") +
+  theme_few() +
+  labs(x = "Mean proboscis length (mm)",
+       y = "Mean corolla length (mm)")
+
+## To get regression formula shown in figure 5.
+## Comes very close to actual value.
+bee_lm <- lm(corolla ~ proboscis, data = prob_raw)
+
+summary(bee_lm)
