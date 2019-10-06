b0bc87695c697be2aff233b010c7fd1e776d64e4
Michael S. Taylor (home) <mtaylor@semo.edu>
2019-10-06 06:42:13 -0500

    Linear regression and ANOVA.

---
+++ bombus_stats.R
@@ -0,0 +1,37 @@
+## Script to perform various statistical analysis.
+## These may be incorporated into Shiny apps for 
+## students to statistically analyze the data.
+
+library(tidyverse)
+
+# ANOVA of proboscis lengths ----------------------------------------------
+#
+
+proboscis_lengths <- read_csv("proboscis_lengths.csv") %>% 
+  gather(key = species, 
+         value = length)
+
+proboscis_aov <- aov(length ~ species, data = proboscis_lengths)
+
+summary(proboscis_aov)
+
+## Alternative form
+
+proboscis_lm <- lm(length ~ species, data = proboscis_lengths)
+proboscis_aov <- aov(proboscis_lm)
+proboscis_tukey <- TukeyHSD(proboscis_aov)
+
+# Linear regression of proboscis and corolla lengths ----------------------
+#
+
+pc_length <- read_csv("proboscis_corolla_lengths.csv")
+
+pc_model <- lm(corolla ~ proboscis, data = pc_length)
+pc_aov <- aov(pc_model)
+
+
+summary(pc_model)
+
+summary(pc_aov)
+
+
